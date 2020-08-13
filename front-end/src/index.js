fa5d75783ba2630116e2bf3f6048fb1a86b6fa7e
Arnold Makarov <arnoldmakarov@yahoo.com>
2020-08-13 14:50:15 -0400

    fixed basename

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -7,7 +7,7 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={"https://rocksrock18.github.io/"}>
         <Home />
     </BrowserRouter>
     , document.getElementById('root'));
